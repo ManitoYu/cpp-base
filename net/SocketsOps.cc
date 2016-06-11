2baf7185255ac2c5949570b0fa9c289aa222b5fa
onionkings <412950798@qq.com>
2016-06-11 20:19:41 +0800

    修改SocketsOps文件，增加readv方法

--- net/SocketsOps.cc
+++ net/SocketsOps.cc
@@ -61,6 +61,10 @@ ssize_t sockets::read(int sockfd, void* buf, size_t count) {
   return ::read(sockfd, buf, count);
 }
 
+ssize_t sockets::readv(int sockfd, const struct iovec* iov, int iovcnt) {
+  return ::readv(sockfd, iov,iovcnt);
+}
+
 ssize_t sockets::write(int sockfd, void* buf, size_t count) {
   return ::write(sockfd, buf, count);
 }
--- net/SocketsOps.h
+++ net/SocketsOps.h
@@ -13,6 +13,7 @@ void listenOrDie(int sockfd);
 int accept(int sockfd, struct sockaddr_in* addr);
 int connect(int sockfd, const struct sockaddr* addr);
 ssize_t read(int sockfd, void* buf, size_t count);
+ssize_t readv(int sockfd, const struct iovec* iov, int iovcnt);
 ssize_t write(int sockfd, void* buf, size_t count);
 void close(int sockfd);
 void shutdownWrite(int sockfd);
