6ab2cbec871487dd522f4ec600be928441be717d
onionkings <412950798@qq.com>
2016-06-11 20:23:46 +0800

    修改Callbacks文件，增加ConnectionCallback，CloseCallback，MessageCallback，defaultConnectionCallback，defaultMessageCallback回调函数定义

--- net/Callbacks.h
+++ net/Callbacks.h
@@ -2,11 +2,21 @@
 #define NET_CALLBACKS_H
 
 #include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+#include <base/Timestamp.h>
 
 namespace base {
 namespace net {
 
+class TcpConnection;
+typedef boost::shared_ptr<TcpConnection> TcpConnectionPtr;
 typedef boost::function<void ()> TimerCallback;
+typedef boost::function<void (const TcpConnectionPtr&)> ConnectionCallback;
+typedef boost::function<void (const TcpConnectionPtr&)> CloseCallback;
+typedef boost::function<void (const TcpConnectionPtr&, char*, Timestamp)> MessageCallback;
+
+void defaultConnectionCallback(const TcpConnectionPtr& conn);
+void defaultMessageCallback(const TcpConnectionPtr& conn, char* buf, Timestamp receiveTime);
 
 }
 }
