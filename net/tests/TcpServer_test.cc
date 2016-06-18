288481ace311e9d078d16dbf4c471ae2857be09e
onionkings <412950798@qq.com>
2016-06-18 16:16:26 +0800

    修改TcpServer测试文件

--- net/tests/TcpServer_test.cc
+++ net/tests/TcpServer_test.cc
@@ -1,15 +1,29 @@
 #include <net/TcpServer.h>
 #include <net/EventLoop.h>
 #include <net/InetAddress.h>
+#include <base/Logging.h>
 
 using namespace base;
 using namespace base::net;
 
 int main() {
+  Logger::setLogLevel(Logger::TRACE);
+
   EventLoop loop;
   InetAddress addr(3000, true);
 
   TcpServer server(&loop, addr, "TestServer");
+  server.setConnectionCallback([](const TcpConnectionPtr& conn) {
+    LOG_INFO << "a new connection";
+  });
+  server.setMessageCallback([](const TcpConnectionPtr& conn, Buffer* buf, Timestamp timestamp) {
+    // LOG_INFO << buf->retrieveAllAsString();
+    conn->send(buf);
+  });
+  server.setWriteCompleteCallback([](const TcpConnectionPtr& conn) {
+
+  });
+
   server.start();
 
   loop.loop();
